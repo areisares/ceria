e7ddb2964ca4b2c301af8c4bbeddf998e76ff8f3
Areis Indrawan <99317800+areisares@users.noreply.github.com>
2025-05-10 01:58:14 +0700

    Add files via upload

---
+++ ceria-app/soal-huruf-final.js
@@ -0,0 +1,236 @@
+const soal = [
+  {
+    "gambar": "X.png",
+    "jawaban": "X",
+    "pilihan": [
+      "X",
+      "I",
+      "A"
+    ]
+  },
+  {
+    "gambar": "J.png",
+    "jawaban": "J",
+    "pilihan": [
+      "M",
+      "J",
+      "R"
+    ]
+  },
+  {
+    "gambar": "T.png",
+    "jawaban": "T",
+    "pilihan": [
+      "K",
+      "R",
+      "T"
+    ]
+  },
+  {
+    "gambar": "N.png",
+    "jawaban": "N",
+    "pilihan": [
+      "V",
+      "G",
+      "N"
+    ]
+  },
+  {
+    "gambar": "O.png",
+    "jawaban": "O",
+    "pilihan": [
+      "O",
+      "S",
+      "K"
+    ]
+  },
+  {
+    "gambar": "A.png",
+    "jawaban": "A",
+    "pilihan": [
+      "A",
+      "Y",
+      "R"
+    ]
+  },
+  {
+    "gambar": "I.png",
+    "jawaban": "I",
+    "pilihan": [
+      "B",
+      "E",
+      "I"
+    ]
+  },
+  {
+    "gambar": "W.png",
+    "jawaban": "W",
+    "pilihan": [
+      "D",
+      "R",
+      "W"
+    ]
+  },
+  {
+    "gambar": "C.png",
+    "jawaban": "C",
+    "pilihan": [
+      "C",
+      "J",
+      "T"
+    ]
+  },
+  {
+    "gambar": "Y.png",
+    "jawaban": "Y",
+    "pilihan": [
+      "N",
+      "Y",
+      "J"
+    ]
+  },
+  {
+    "gambar": "D.png",
+    "jawaban": "D",
+    "pilihan": [
+      "D",
+      "A",
+      "N"
+    ]
+  },
+  {
+    "gambar": "K.png",
+    "jawaban": "K",
+    "pilihan": [
+      "F",
+      "K",
+      "G"
+    ]
+  },
+  {
+    "gambar": "S.png",
+    "jawaban": "S",
+    "pilihan": [
+      "B",
+      "S",
+      "M"
+    ]
+  },
+  {
+    "gambar": "M.png",
+    "jawaban": "M",
+    "pilihan": [
+      "K",
+      "F",
+      "M"
+    ]
+  },
+  {
+    "gambar": "E.png",
+    "jawaban": "E",
+    "pilihan": [
+      "E",
+      "X",
+      "Z"
+    ]
+  },
+  {
+    "gambar": "L.png",
+    "jawaban": "L",
+    "pilihan": [
+      "J",
+      "L",
+      "M"
+    ]
+  },
+  {
+    "gambar": "F.png",
+    "jawaban": "F",
+    "pilihan": [
+      "F",
+      "W",
+      "I"
+    ]
+  },
+  {
+    "gambar": "P.png",
+    "jawaban": "P",
+    "pilihan": [
+      "L",
+      "P",
+      "T"
+    ]
+  },
+  {
+    "gambar": "Z.png",
+    "jawaban": "Z",
+    "pilihan": [
+      "Z",
+      "K",
+      "U"
+    ]
+  },
+  {
+    "gambar": "H.png",
+    "jawaban": "H",
+    "pilihan": [
+      "R",
+      "H",
+      "N"
+    ]
+  },
+  {
+    "gambar": "U.png",
+    "jawaban": "U",
+    "pilihan": [
+      "B",
+      "A",
+      "U"
+    ]
+  },
+  {
+    "gambar": "V.png",
+    "jawaban": "V",
+    "pilihan": [
+      "H",
+      "V",
+      "K"
+    ]
+  },
+  {
+    "gambar": "G.png",
+    "jawaban": "G",
+    "pilihan": [
+      "E",
+      "G",
+      "T"
+    ]
+  },
+  {
+    "gambar": "B.png",
+    "jawaban": "B",
+    "pilihan": [
+      "B",
+      "A",
+      "F"
+    ]
+  },
+  {
+    "gambar": "R.png",
+    "jawaban": "R",
+    "pilihan": [
+      "O",
+      "R",
+      "X"
+    ]
+  },
+  {
+    "gambar": "Q.png",
+    "jawaban": "Q",
+    "pilihan": [
+      "C",
+      "Z",
+      "Q"
+    ]
+  }
+];
